2a6610f58d5c2799658d22e266e805e6eb2a4aa1
damienlabbe <damienlabbe69@gmail.com>
2020-03-09 14:36:08 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,72 @@
-import React from 'react';
-import ReactDom from 'react-dom';
 
-import sayHello from './utils';
+import React, { useState, useEffect } from 'react';
+import ReactDom from 'react-dom';
+import countYearsSince from 'damienlabbecountyearssince';
+import axios from 'axios';
+
 import './style.css';
 
-const Content = () => (
-    <div>
-        <h1>Test</h1>
-        <p>{sayHello()}</p>
-    </div>
-);
+const Content = () => {
+    const [ tab, setTab ] = useState('formation');
+    const [ img, setImg ] = useState('');
 
+    useEffect(() => { 
+        axios.get('https://aws.random.cat/meow')
+            .then((response) => {
+                setImg(response.data.file);
+            });
+    }, []);
+
+    const generateClickHandler = (newTab) => () => {
+        setTab(newTab);
+    }
+    return (
+        <main>
+            <header>
+                {img !== '' && <img src={img} alt="Mon CV en ligne" />}
+                <h1>CV de Damien LABBE</h1>
+            </header>
+
+            <section>
+                <h2>Je suis développeur depuis {countYearsSince(2020)} ans</h2>
+                <div>
+                    <button onClick={generateClickHandler('formation')}>Formations</button>
+                    <button onClick={generateClickHandler('experience')}>Expériences</button>
+                    <button onClick={generateClickHandler('autre')}>Autre</button>
+                </div>
+                {tab === 'formation' && ( 
+                    <article>
+                        <h3>Formations</h3>
+                        <ul>
+                            <li>J'ai un BAC S spé physique-chimie</li>
+                            <li>J'ai suivi une formation developpeur web fullstack JS spécialisation REACT chez O'clock</li>
+                            <li>Je passe actuellement le titre professionel developpeur web</li>
+                        </ul>
+                    </article>
+                )}
+               
+                {tab === 'experience' && ( 
+                    <article>
+                        <h3>Expériences</h3>
+                        <ul>
+                            <li>Je suis en formation chez Oclock depuis {countYearsSince(2019)} ans</li>
+                            <li>J'étais gérant d'une société pendant 8 ans</li>
+                            <li>Et l'année avant ma formation, j'étais en intérim</li>
+                        </ul>
+                    </article>
+                )}
+                {tab === 'autre' && ( 
+                    <article>
+                        <h3>Autre</h3>
+                        <ul>
+                            <li>J'ai le permis B</li>
+                            <li>J'aime bien geeker</li>
+                            <li>J'aime bien bouquiner</li>
+                        </ul>
+                    </article>
+                )}
+            </section>
+        </main>
+    );
+};
 ReactDom.render(<Content />, document.getElementById('root'));
